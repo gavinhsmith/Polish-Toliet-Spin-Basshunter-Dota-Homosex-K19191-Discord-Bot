7fda25cfb388a0a6cf6b7e83361805aa7bc4de7e
gavinhsmith <gavindagrate101@gmail.com>
2020-11-14 18:04:43 -0600

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ bot.on('ready', () => {
 bot.on("message", async msg => {
     if (msg.author.bot) return;
     if (msg.content.slice(msg.content.indexOf(":"), msg.content.lastIndexOf(":")+1) === `:homosex_gif:` || msg.content.slice(msg.content.indexOf(":"), msg.content.lastIndexOf(":")+1) === `:homosex:`) {
-        console.log(`H O M O   S E X   A C T I V A T E D`)
+        console.log(`HOMO SEX   A C T I V A T E D`);
         if (msg.member.voice.channel) {
             const vc_connection = await msg.member.voice.channel.join();
             const dispatcher = vc_connection.play("homosex.mp3");
